3dd0c269783ae4354f7c36d3ab4039cf3c4d122d
hzz <dexter.ho.cn@gmail.com>
2021-07-30 17:08:02 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,11 @@
+module go-click
+
+go 1.16
+
+require (
+	github.com/StackExchange/wmi v1.2.1 // indirect
+	github.com/go-vgo/robotgo v0.99.0
+	github.com/shirou/gopsutil v3.21.6+incompatible // indirect
+	github.com/tklauser/go-sysconf v0.3.7 // indirect
+	golang.org/x/image v0.0.0-20210628002857-a66eb6448b8d // indirect
+)
---
+++ main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/go-vgo/robotgo"
+	"log"
+	"os/exec"
+	"strconv"
+)
+
+var  (
+	scroll = 0
+	cx = 79
+	cy = 670
+	path = "D:\\Trojan-Qt5-Windows\\trojan-qt5.exe"
+)
+
+func init()  {
+	// 初始化参数
+	_path := *flag.String("path", "D:\\Trojan-Qt5-Windows\\trojan-qt5.exe", "trojan-qt5.exe的绝对路径")
+	_scroll := *flag.String("scroll", "0", "鼠标X轴方向向下滚动的值，按需设置")
+	_cx := *flag.String("cx", "79", "连接节点的X轴坐标")
+	_cy := *flag.String("cy", "670", "连接节点的Y轴坐标")
+	flag.Parse()
+
+	intScroll ,_ := strconv.Atoi(_scroll)
+	intX ,_ := strconv.Atoi(_cx)
+	intY ,_ := strconv.Atoi(_cy)
+	scroll = intScroll
+	cx = intX
+	cy = intY
+	path = _path
+
+}
+
+func main() {
+
+	//exec.Command("cmd", "/c", "start", "www.baidu.com").()
+	log.Println("启动 trojan-qt5 ")
+	// -windowstyle Maximized
+	runErr := exec.Command("cmd", "/c", "start", "D:\\Trojan-Qt5-Windows\\trojan-qt5.exe").Run()
+	if runErr != nil {
+		panic("启动 trojan-qt5 失败 " + runErr.Error())
+	}
+	log.Println("等待锁定当前窗口")
+	//等待
+	robotgo.MilliSleep(3 * 1000)
+
+	// trojan-qt5
+	//fpid, err := robotgo.FindIds("trojan-qt5")
+	//log.Println("fpid, err",fpid, err)
+
+	title := robotgo.GetTitle()
+	fmt.Println("title-----", title)
+
+	// get current Window Active
+	mdata := robotgo.GetActive()
+	// set Window Active
+	robotgo.SetActive(mdata)
+
+	robotgo.MilliSleep(1 * 1000)
+	// 全屏
+	x := robotgo.KeyTap(`x`,`alt`, `space`)
+	y := robotgo.KeyTap(`esc`, `ctrl`, `shift`)
+	fmt.Println("x-----", x,y)
+
+	// 获取鼠标初始坐标
+	ix, iy := robotgo.GetMousePos()
+	log.Println("获取鼠标初始坐标",ix, iy)
+
+	if scroll != 0{
+		// 鼠标移动到最小可滑动位置
+		robotgo.MoveMouse(420, 320)
+		robotgo.ScrollMouse(scroll, "down")
+	}
+
+	// 鼠标移动到要连接的节点坐标位置
+	robotgo.MoveMouse(cx, cy)
+	// 双击鼠标左键
+	robotgo.MouseClick("left", true)
+	log.Println("连接节点",cx, cy)
+
+	// close current Window
+	//robotgo.CloseWindow()
+	log.Println("关闭 trojan-qt5 主页面")
+
+	// 鼠标回归初始位置
+	robotgo.MoveMouse(ix, iy)
+	log.Println("鼠标回归初始位置，程序完成")
+}
